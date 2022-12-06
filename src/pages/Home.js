e395cdd88dc960fea9584759a1472193c7698ccc
Felix Di Luciano <centenofelix98@gmail.com>
2022-12-06 03:00:40 -0300

    changed home app full done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import Container from "@mui/material/Container";
 import BigButton from "../components/BigButton";
 import H1 from '../components/H1'
 import H2 from '../components/H2'
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 function Home() {
